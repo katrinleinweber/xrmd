517ce1d75c052534ed59197b54965402ae472105
Simon Harper <simon.harper@manchester.ac.uk>
2015-06-18 13:30:50 +0100

    Check in the verbose version

--- xr.lua
+++ xr.lua
@@ -46,23 +46,35 @@ xrLazyTargetKey =
 xrTargetKeys = {}
 
 xrTypes = {}
+
+--[[
+
 xrTypes["fig"]={ ["Name"]      = "Figure", 
                     ["Counter"]   = 1, 
-                    ["Inline"]    = "%!%[%a%a%a%:[%w%_*%-*]+.+%]%((.-)([^\\/]-%.?([^%.\\/]*))[%s+\"*\'*.\"*\'*]*%)", 
-                    ["RefInline"] = "%!%[.+%]%[%a%a%a%:[%w%_*%-*]+%]", 
-                    ["RefEnd"]    = "%[%a%a%a%:[%w%_*%-*]+%]%:%s+(.-)([^\\/]-%.?([^%.\\/]*))%s+[\"*\'*.\"*\'*]*%s*[width=[\"*\'*.\"*\'*]]?",
+                    ["Inline"]    = "%!%[fig:[%w%_*%-*]+.+%]%((.-)([^\\/]-%.?([^%.\\/]*))[%s+\"*\'*.\"*\'*]*%)", 
+                    ["RefInline"] = "%!%[.+%]%[fig:[%w%_*%-*]+%]", 
+                    ["RefEnd"]    = "%[fig:[%w%_*%-*]+%]%:%s+(.-)([^\\/]-%.?([^%.\\/]*))%s+[\"*\'*.\"*\'*]*%s*[width=[\"*\'*.\"*\'*\]\]?",
                     ["Target"]    = "%[fig:[%w%_*%-*]+%]" 
                   }
+                ]]
+                
+xrTypes["fig"]={ ["Name"]      = "Figure", 
+                    ["Counter"]   = 1, 
+                    ["Inline"]    = "%!%[fig:%g+%s.+%]%(.+%)", 
+                    ["RefInline"] = "%!%[.+%]%[fig:[%w%_*%-*]+%]", 
+                    ["RefEnd"]    = "%[fig:[%w%_*%-*]+%]%:%s+(.-)([^\\/]-%.?([^%.\\/]*))%s+[\"*\'*.\"*\'*]*%s*[width=[\"*\'*.\"*\'*]]?",
+                    ["Target"]    = "%[fig:[%w%_*%-*]+%]" 
+                  }                
                   
 --xrTypes["tab"]={["Name"]="Table",  ["Counter"]=1}
 
 -- Build List of all Figures Increamenting as you go.
 for xrType in pairs(xrTypes) do
   for sLine in io.lines(sInputfile) do 
-    for TempID in string.gmatch(sLine, xrTypes.fig.Target) do
-      ID = TempID:sub(2, -2)
-      if xrTargetKeys[ID] == nil then -- Don't Store Duplicates  
-        xrTargetKeys[ID] = xrTypes.fig.Counter
+    for tempID in string.gmatch(sLine, xrTypes.fig.Target) do
+      findID = tempID:sub(2, -2)
+      if xrTargetKeys[findID] == nil then -- Don't Store Duplicates  
+        xrTargetKeys[findID] = xrTypes.fig.Counter
         xrTypes.fig.Counter = xrTypes.fig.Counter+1
       end  
     end
@@ -72,10 +84,34 @@ end
 
 file = io.open(("xr"..sInputfile), "w") -- List is build Now Open new file for writing to same output.
 
---[[
+
+
 for sLine in io.lines(sInputfile) do 
-  for sTemp in string.gmatch(sLine, xrmdRefKey) do
-    s = sTemp:sub(2, -3)
+  if(string.match(sLine, "fig:[%w%_*%-*]+")) then 
+    sLine = string.gsub(sLine, "fig:[%w%_*%-*]+", xrTypes.fig.Name.." "..xrTargetKeys[string.match(sLine, "fig:[%w%_*%-*]+")])
+    sLine = string.gsub(sLine, "\"", "\""..substituteString..": ", 1) 
+
+    
+  end
+  
+  
+  --[[if(string.match(sLine, xrTypes.fig.Inline)) then -- Let's do the inline figures
+    replaceID=sLine:sub(string.find(sLine, xrTypes.fig.Inline))
+    substituteString = xrTypes.fig.Name.." "..xrTargetKeys[replaceID:sub(3,-3)]
+    sLine = string.gsub(sLine, xrTypes.fig.Inline, "!["..substituteString..": ")
+    sLine = string.gsub(sLine, "\"", "\""..substituteString..": ", 1) 
+  elseif(string.match(sLine, xrTypes.fig.Inline)) then
+  
+  
+  end
+  ]]
+  file:write(sLine, "\n")
+end
+
+file:close()  -- Close it
+
+--[[for sTemp in string.gmatch(sLine, xrTypes.fig.Inline) do
+    s = sTemp:sub(2, 5)
     if xrTargetKeys[s] then -- Got to Be Listed  
       if s:sub(1,3) == "fig" then
         substituteString="Figure "..xrTargetKeys[s]
@@ -84,10 +120,4 @@ for sLine in io.lines(sInputfile) do
       end
       sLine = string.gsub(sLine, "%["..s.."%]", substituteString)
     end  
-  end
-  file:write(sLine, "\n")
-end
-]]
-
-file:close()  -- Close it
-
+  end]]
